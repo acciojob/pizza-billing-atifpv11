d923f601212e8f7a0e9c95f3c4287e314d32a1ff
atifpv11 <muhammedatif44@gmail.com>
2024-02-27 23:20:47 +0530

    clarfied changes

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -34,15 +34,31 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+        if(!flagCheese)
+            this.price+=80;
         flagCheese=true;
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(!flagToppings)
+        {
+            if(isVeg)
+            {
+                this.price+=70;
+            }
+            else
+            {
+                this.price+=120;
+            }
+        }
         flagToppings=true;
     }
 
     public void addTakeaway(){
+        if(!flagTakeaway)
+            this.price+=20;
         flagTakeaway=true;
     }
 
@@ -50,25 +66,21 @@ public class Pizza {
         // your code goes here
         if(flagCheese)
         {
-            this.price+=80;
             this.bill+="Extra Cheese Added: 80\n";
         }
         if(flagToppings)
         {
             if(isVeg)
             {
-                this.price+=70;
                 this.bill+="Extra Toppings Added: 70\n";
             }
             else
             {
-                this.price+=120;
                 this.bill+="Extra Toppings Added: 120\n";
             }
         }
         if(flagTakeaway)
         {
-            this.price+=20;
             this.bill+="Paperbag Added: 20\n";
         }
         this.bill+="Total Price: "+this.price;
